9ef285c6359f44b679a723c6901d59d3bea46a09
pavlen5kiy <150275009+pavlen5kiy@users.noreply.github.com>
2024-05-10 23:05:32 +0300

    Biomes extremely modified

--- static/script.js
+++ static/script.js
@@ -19,7 +19,7 @@ let offsetX = 0;
 let offsetY = 0;
 
 // Function to generate Perlin noise
-function perlinNoise(x, y) {
+function terrainMap(x, y) {
   let total = 0;
   let frequency = 0.5;
   let amplitude = 0.25;
@@ -28,8 +28,8 @@ function perlinNoise(x, y) {
   for (let i = 0; i < octaves; i++) {
     total +=
       noise.simplex2(
-        (x + offsetX) * frequency * noiseScale,
-        (y + offsetY) * frequency * noiseScale
+        (x + offsetX - 200) * frequency * noiseScale,
+        (y + offsetY - 200) * frequency * noiseScale
       ) * amplitude;
 
     maxValue += amplitude;
@@ -50,8 +50,8 @@ function temperatureMap(x, y) {
   for (let i = 0; i < octaves / 4; i++) {
     total +=
       noise.simplex2(
-        (x + offsetX) * frequency * noiseScale * 0.12,
-        (y + offsetY) * frequency * noiseScale * 0.12
+        (x + offsetX - 200) * frequency * noiseScale * 0.12,
+        (y + offsetY - 200) * frequency * noiseScale * 0.12
       ) * amplitude;
 
     maxValue += amplitude;
@@ -65,15 +65,15 @@ function temperatureMap(x, y) {
 
 function moistureMap(x, y) {
   let total = 0;
-  let frequency = 0.5;
+  let frequency = 0.25;
   let amplitude = 0.1;
   let maxValue = 0; // Used for normalizing result to 0.0 - 1.0
 
   for (let i = 0; i < octaves / 2; i++) {
     total +=
       noise.simplex2(
-        (x + offsetX) * frequency * noiseScale * 0.5,
-        (y + offsetY) * frequency * noiseScale * 0.5
+        (x + offsetX - 200) * frequency * noiseScale * 0.06,
+        (y + offsetY - 200) * frequency * noiseScale * 0.06
       ) * amplitude;
 
     maxValue += amplitude;
@@ -89,50 +89,71 @@ function determineBiome(elevation, temperature, moisture) {
   if (elevation < 0.1) {
     return "Water";
   } else if (elevation < 0.2) {
-    if (temperature > -0.75) {
+    if (temperature > 0) {
       return "Desert";
-    } else {
+    } else if (temperature > -0.5) {
       return "Mountains"; // not mountains, but stoney beach or sth
+    } else {
+      return "IcyBeach";
     }
   } else if (elevation < 0.4) {
-    if (moisture < 0.5) {
-      if (temperature > 0.2) {
-        return "Desert";
-      } else if (temperature > -0.5) {
-        return "Plains";
+    if (temperature > 0.5) {
+      if (moisture > 0.5) {
+        return "Jungle";
+      } else if (moisture > 0) {
+        return "Savanna";
+      } else if (moisture > -0.5) {
+        return "RedDesert";
       } else {
-        return "SnowyPlains";
+        return "Desert";
       }
-    } else {
-      if (temperature > 0.5) {
-        if (moisture > 0.5) {
-          return "Jungle";
-        } else {
-          return "Savanna";
-        }
-      } else if (temperature > -0.5) {
+    } else if (temperature > -0.5) {
+      if (moisture > 0.5) {
+        return "Swamp";
+      } else if (moisture > 0) {
         return "Forest";
       } else {
+        return "Plains";
+      }
+    } else {
+      if (moisture > 0.5) {
         return "Taiga";
+      } else {
+        return "SnowyPlains";
       }
     }
   } else if (elevation < 0.6) {
     if (temperature > 0.5) {
-      return "Jungle";
-    } else if (temperature > 0) {
-      return "Savanna";
+      if (moisture > 0.5) {
+        return "MountainsJungle";
+      } else if (moisture > 0) {
+        return "MountainsSavanna";
+      } else if (moisture > -0.5) {
+        return "Badlands";
+      }
     } else if (temperature > -0.5) {
       return "Forest";
     } else {
-      return "Taiga";
+      return "MountainsTaiga";
     }
   } else if (elevation < 0.75) {
     if (temperature > 0.5) {
-      return "Forest";
+      if (moisture > 0.5) {
+        return "MountainsJungle";
+      } else if (moisture > 0) {
+        return "MountainsSavanna";
+      } else if (moisture > -0.5) {
+        return "Mountains";
+      }
+    } else if (temperature > 0) {
+      return "MountainsForest";
     } else {
       return "Mountains";
     }
   } else {
+    if (temperature > 0.5) {
+      return "Mountains";
+    }
     return "SnowyMountains";
   }
 }
@@ -147,17 +168,33 @@ function determineColor(biome, elevation, oceanTemperature) {
     case "Plains":
       return "rgb(127, 204, 25)";
     case "Forest":
-      return "rgb(0, 87, 0)";
+      return "rgb(0, 124, 0)";
+    case "MountainsForest":
+      return "rgba(0, 87, 0)";
     case "Mountains":
       return "rgb(141, 144, 158)";
     case "Desert":
       return "rgb(247, 233, 163)";
     case "Jungle":
       return "rgb(0, 217, 58)";
+    case "MountainsJungle":
+      return "rgb(0, 153, 40)";
     case "Savanna":
       return "rgb(103, 117, 53)";
+    case "MountainsSavanna":
+      return "rgb(72, 82, 37)";
     case "Taiga":
       return "rgb(22, 126, 134)";
+    case "MountainsTaiga":
+      return "rgb(15, 88, 94)";
+    case "IcyBeach":
+      return "rgb(102, 153, 216)";
+    case "Badlands":
+      return "rgb(137, 70, 31)";
+    case "RedDesert":
+      return "rgb(159, 82,36)";
+    case "Swamp":
+      return "rgb(53, 57, 29)";
     default:
       return "rgb(255, 255, 255)";
   }
@@ -176,7 +213,7 @@ function generateTerrainWithBiomes() {
 
   for (let y = 0; y < height; y++) {
     for (let x = 0; x < width; x++) {
-      const elevation = perlinNoise(x, y);
+      const elevation = terrainMap(x, y);
       const temperature = temperatureMap(x, y);
       const moisture = moistureMap(x, y);
 
@@ -192,26 +229,32 @@ generateTerrainWithBiomes();
 
 // Handle arrow key press events
 document.addEventListener("keydown", function (event) {
+  const moveSpeed = 20;
+  const zoomInParam = 0.98;
+  const zoomOutParam = 1.02;
+
   switch (event.key) {
     case "w":
-      offsetY -= 5;
+      offsetY -= moveSpeed;
       break;
     case "s":
-      offsetY += 5;
+      offsetY += moveSpeed;
       break;
     case "a":
-      offsetX -= 5;
+      offsetX -= moveSpeed;
       break;
     case "d":
-      offsetX += 5;
+      offsetX += moveSpeed;
       break;
     case "[":
       // Adjust noise scale for zooming out
-      noiseScale *= 1.02; // Zoom out
+      noiseScale *= zoomOutParam; // Zoom out
+
       break;
     case "]":
       // Adjust noise scale for zooming in
-      noiseScale *= 0.98; // Zoom in
+      noiseScale *= zoomInParam; // Zoom in
+
       break;
     default:
       return; // Exit this handler for other keys
